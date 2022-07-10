cf8fc5b20f4b2b65c70deb8195ea9a89afa6efeb
Rebeca Guimaraes <106562306+arebecag@users.noreply.github.com>
2022-07-10 11:22:25 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Livros livros =new Livros();
+		
+		System.out.println(livros);
+		
+		/*System.out.println("hello world");*/
+	}
+}
+
+	class Livros {
+		private String nome;
+		private String npag;
+		
+	}
+
+
+
